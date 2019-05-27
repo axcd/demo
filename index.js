4adf6a2e3b8e722dff0adae886192eab69083ad3
axcd <maoyuyin@126.com>
2019-05-27 12:25:41 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+var str = navigator.userAgent;
+var re = new RegExp("Mobile");
 
-document.write("<p>UserAgent: ")
-document.write(navigator.userAgent + "</p>")
-document.write(navigator.userAgent.match("Mobile")+ "@------</p>")
+var result = re.test(str);
+
+if(result){
+  location.href="https://axcd.github.io/web/";
+}else{
+  document.write("这是一个手机网页");
+
+}
 
 
 $(function(){
